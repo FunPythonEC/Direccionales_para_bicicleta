175c43d0f75b8aa33a40d232b44387764fe1e371
sasilva1998 <30530787+sasilva1998@users.noreply.github.com>
2018-10-02 20:27:44 -0500

    Update direccbicicleta.py

--- Codigos/direccbicicleta.py
+++ Codigos/direccbicicleta.py
@@ -1,29 +1,35 @@
 """
 Programado en la versión 1.9 de MicroPython
+Tener en cuenta de que si se usa otra versión de MicroPython
+es posible de que se tenga que modificar cierta función.
+Además de que el código puede variar dependiendo del largo de la tira led.
 """
+
+#importamos neopixel ya que es la que sirve para poder manejar los leds.
 from machine import Pin
 import machine, neopixel
 import time
 
-switch_derecha= Pin(5, Pin.IN) #derecha cable VERDE
-switch_izquierda= Pin(4, Pin.IN) #izquierda cable AZUL
+switch_derecha= Pin(5, Pin.IN) #se define los pines para poder detectar cuando el pulsador es aplastado.
+switch_izquierda= Pin(4, Pin.IN)
 
-num_leds=19
-pin_salida=12
+num_leds=19 #numero de leds de la tira usada, para asegurarnos que usamos todos los leds.
+pin_salida=12 #el pin de salida puede cambiar dependiendo de cada persona.
 np = neopixel.NeoPixel(machine.Pin(pin_salida), num_leds)
 
+#función para poder hacer el patron hacia la derecha en los leds.
 def derecha():
     for led in range(0,num_leds - 9,1):
-        np[led]=(255,035,001)
+        np[led]=(255,35,1)
         np.write()
         time.sleep_ms(50)
         print ('derecha')
     apagar()
     
-    
+#patrón hacia la izquierda en los leds
 def izquierda():
     for led in range(0,num_leds - 9,1):
-        np[9-led]=(255,035,001)
+        np[9-led]=(255,35,1)
         np.write()
         time.sleep_ms(50)
         print ('izquierda')
